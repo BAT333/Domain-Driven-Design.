2ffe71417370884ff8bdf27a0d48d47b473aeb7b
Rafael alves oliveira <87524816+BAT333@users.noreply.github.com>
2024-06-22 17:57:42 -0300

    Update Evento.java

--- Domain-Driven-Design/src/main/java/br/com/alura/codechella/Domain/Evento/Evento.java
+++ Domain-Driven-Design/src/main/java/br/com/alura/codechella/Domain/Evento/Evento.java
@@ -35,6 +35,41 @@ public class Evento {
     public void incluiNovoTipoIngressoAoEvento (TipoIngresso tipoIngresso){
         this.tipoIngressos.add(tipoIngresso);
     }
+      }
+
+    public static class Builder {
+        private Evento evento;
+
+        public Builder() {
+            evento = new Evento();
+        }
+
+        public Builder comCategoria(Categoria categoria) {
+            evento.categoria = categoria;
+            return this;
+        }
+
+        public Builder comDescricao(String descricao) {
+            evento.descricao = descricao;
+            return this;
+        }
+
+        public Builder comEndereco(String cep, Integer numero, String complemento) {
+            Endereco endereco = new Endereco(cep, numero, complemento);
+            evento.endereco = endereco;
+            return this;
+        }
+
+        public Builder comData(LocalDateTime data) {
+            evento.data = data;
+            return this;
+        }
+
+        public Evento build() {
+            evento.gerarIdentificadorUnico();
+            return evento;
+        }
+    }
 
     public Long getId() {
         return id;
